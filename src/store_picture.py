adde5402b9c98cad420bd084a0194ac09fbe3ddc
odan <odan3240@gmail.com>
2017-12-09 16:08:25 +0900

    投稿されたチャンネルに返信するようにした

--- src/store_picture.py
+++ src/store_picture.py
@@ -14,39 +14,41 @@ class SlackBotMain:
     email = os.environ["EMAIL"]
     password = os.environ["PASSWORD"]
     token = os.environ["SLACK_API_TOKEN"]
-    channel = "team7-playground"
     sc = SlackClient(token)
 
     def __init__(self):
         if SlackBotMain.sc.rtm_connect():
-            self.uploader = EmojiUploader(self.workspace, self.email, self.password)
+            self.uploader = EmojiUploader(
+                self.workspace, self.email, self.password)
             while True:
-                data = SlackBotMain.sc.rtm_read()
+                data_list = SlackBotMain.sc.rtm_read()
 
-                if len(data) > 0:
-                    for item in data:
-                        SlackBotMain.sc.rtm_send_message(self.channel, self.create_message(item))
+                for data in data_list:
+                    if "type" in data and data["type"] == "message":
+                        channel = data['channel']
+                        print(channel)
+                        SlackBotMain.sc.rtm_send_message(
+                            channel, self.create_message(data))
 
                 time.sleep(1)
         else:
             print("Connection Failed, invalid token?")
 
     def create_message(self, data):
-        if "type" in data.keys():
-            if data["type"] == "message":
-                if 'file' in data:
-                    url = data['file']['url_private']
-                    filename = data['file']['title']
-                    token = self.token
-                    image = requests.get(url, headers={'Authorization': 'Bearer %s' % token}, stream=True)
+        if 'file' in data:
+            url = data['file']['url_private']
+            filename = data['file']['title']
+            token = self.token
+            image = requests.get(
+                url, headers={'Authorization': 'Bearer %s' % token}, stream=True)
 
-                    if os.path.exists(filename):
-                        return "<@" + data["user"] + "> " + u"Error: exists file: wait a time or rename upload file"
+            if os.path.exists(filename):
+                return "<@" + data["user"] + "> " + u"Error: exists file: wait a time or rename upload file"
 
-                    with open(filename, 'wb') as myfile:
-                        for chunk in image.iter_content(chunk_size=1024):
-                            myfile.write(chunk)
-                    return self.resize_picture(filename)
+            with open(filename, 'wb') as myfile:
+                for chunk in image.iter_content(chunk_size=1024):
+                    myfile.write(chunk)
+            return self.resize_picture(filename)
 
     def resize_picture(self, filename):
         img = Image.open(filename, 'r')
